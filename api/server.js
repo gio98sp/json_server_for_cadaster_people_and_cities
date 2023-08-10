9062e213c4a8659d969f7941824ce0b4080d1563
Giovane Santos <giovane98sp@gmail.com>
2023-08-10 15:02:06 -0300

    update server

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
+    '/*': '/$1',
 }))
 server.use(router)
 server.listen(3000, () => {
